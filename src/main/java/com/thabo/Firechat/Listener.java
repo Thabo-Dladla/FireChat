01fe42f7c5f454a6b0c88cbe8abed183f0be8fa9
Thabo Dladla <lwazisokzin78@gmail.com>
2026-01-21 06:52:50 +0200

    added my cleaned up code

---
+++ src/main/java/com/thabo/Firechat/Listener.java
@@ -0,0 +1,11 @@
+package com.thabo.Firechat;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.sse.EventSource;
+import okhttp3.sse.EventSourceListener;
+import okhttp3.sse.EventSources;
+
+
+
